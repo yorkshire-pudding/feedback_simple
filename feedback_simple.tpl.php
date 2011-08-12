03283b945195aa5fc94bab03d0724aca2af868ee
rump <drupal@seandunaway.com>
2011-08-11 17:58:46 -0700

    Issue 1245908 by rump: Add class support.

--- feedback_simple.tpl.php
+++ feedback_simple.tpl.php
@@ -5,6 +5,7 @@
  *
  * @param $link: link path.
  * @param $align: left or right.
+ * @param $class: classes to apply to anchor tag.
  * @param $top: distance from the top; include unit.
  * @param $alt: alt text.
  * @param $image: image path.
@@ -15,7 +16,7 @@
 <div id='feedback_simple'>
   <a
     href='<?php print $link ?>'
-    class='feedback_simple-<?php print $align ?>'
+    class='feedback_simple-<?php print $align ?> <?php print implode(' ', $class) ?>'
     style='top: <?php print $top ?>; height: <?php print $height ?>; width: <?php print $width ?>;'>
       <img
         alt='<?php print $alt ?>'
