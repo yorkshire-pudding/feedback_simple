7b2dda5bf01e57ede02046a133dd5927ae771315
rump <drupal@seandunaway.com>
2012-10-31 19:04:19 -0700

    Issue #1826198 by rump: Open link in New Window option. Thanks storytellerjeff!

--- feedback_simple.tpl.php
+++ feedback_simple.tpl.php
@@ -5,6 +5,7 @@
  *
  * @param $enabled  true or false.
  * @param $link  link href.
+ * @param $target  link target.
  * @param $class  array of classes to apply to anchor tag.
  * @param $align  left or right.
  * @param $top  distance from the top; include unit.
@@ -21,6 +22,7 @@
 <div id='feedback_simple'>
   <a
     href='<?php print $link ?>'
+    target='<?php print $target ?>'
     class='feedback_simple-<?php print $align ?> <?php print implode(' ', $class) ?>'
     style='top: <?php print $top ?>; height: <?php print $height ?>px; width: <?php print $width ?>px;'>
       <img
